eddf581e3479eb8aeb5ffb61c3a0c226b8e91930
marlainna <marlainna456@gmail.com>
2025-11-15 19:02:09 -0600

    add

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,6 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from flask_jwt_extended import jwt_required, get_jwt
+# Removed: from flask_jwt_extended import jwt_required, get_jwt
 from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
@@ -12,17 +12,18 @@ blp = Blueprint("Items", "items", description="Operations on items")
 
 @blp.route("/item/<int:item_id>")
 class Item(MethodView):
-    @jwt_required()
+    # Removed: @jwt_required()
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         return item
 
-    @jwt_required()
+    # The delete method previously required 'is_admin' and the JWT. 
+    # It now runs without any authentication.
     def delete(self, item_id):
-        jwt = get_jwt()
-        if not jwt.get("is_admin"):
-            abort(401, message="Admin privilege required.")
+        # Removed: jwt = get_jwt()
+        # Removed: if not jwt.get("is_admin"):
+        # Removed:     abort(401, message="Admin privilege required.")
 
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
@@ -48,12 +49,12 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
-    @jwt_required()
+    # Removed: @jwt_required()
     @blp.response(200, ItemSchema(many=True))
     def get(self):
         return ItemModel.query.all()
 
-    @jwt_required()
+    # Removed: @jwt_required()
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
